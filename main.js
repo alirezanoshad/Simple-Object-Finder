2db0526ca9fe961582a7990d9563665293364864
Alireza <iranexplosm@gmail.com>
2024-08-11 15:46:16 +0330

    -js created

---
+++ main.js
@@ -0,0 +1,57 @@
+
+const user1 = {
+    firstName:"lulu",
+    lastName:"meow",
+    phoneNumber:"none of your Shit:)",
+    score:19,
+    age:18,
+    gender:"female",
+    id:"lulu with a B"
+};
+const user2 = {
+    firstName:"johnny",
+    lastName:"soons",
+    phoneNumber:911,
+    score:21,
+    age:45,
+    gender:"male",
+    id:"Smashin it"
+};const user3 = {
+    firstName:"ahmed",
+    lastName:"ghasemi",
+    phoneNumber:19405206911,
+    score:0.25,
+    age:9,
+    gender:"male",
+    id:"ahmedLX"
+};
+const user4 = {
+    firstName:"lily",
+    lastName:"loop",
+    phoneNumber:21212121,
+    score:19.75,
+    age:17,
+    gender:"female",
+    id:"nerdAF"
+};
+const user5 = {
+    firstName:"damon",
+    lastName:"salvatore",
+    phoneNumber:10116985,
+    score:11,
+    age:530,
+    gender:"male",
+    id:"Charming Vamp"
+}
+
+
+function introducing(user){
+    return `${user.firstName} ${user.lastName} is ${user.age} years old`
+}
+
+console.log(user1)
+console.log(user2)
+console.log(user3)
+console.log(user4)
+console.log(user5)
+
